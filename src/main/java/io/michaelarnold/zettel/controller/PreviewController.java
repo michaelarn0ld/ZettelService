ad097a530f029c07835fedda68609e6afdfaf677
Michael Arnold <me@michaelarnold.io>
2023-01-02 16:31:18 -0800

    enable HTTPS on zettel-service

--- src/main/java/io/michaelarnold/zettel/controller/PreviewController.java
+++ src/main/java/io/michaelarnold/zettel/controller/PreviewController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class PreviewController {
     @Autowired
     PreviewService service;
 
+    @CrossOrigin
     @GetMapping(ApplicationConfiguration.PREVIEWS_ENDPOINT)
     public ResponseEntity<?> getPreviews() {
         try {
--- src/main/java/io/michaelarnold/zettel/controller/ZettelFileController.java
+++ src/main/java/io/michaelarnold/zettel/controller/ZettelFileController.java
@@ -9,6 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class ZettelFileController {
     @Autowired
     ZettelFileService service;
 
+    @CrossOrigin
     @GetMapping(ApplicationConfiguration.ZETTEL_FILE_ENDPOINT)
     public ResponseEntity<?> getZettelFile(@PathVariable String zettelId) {
         try {
